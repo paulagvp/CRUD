3ebbf0e70e9fd3c6594fa52571e38de1bf73b0be
Paula <paulagvp@gmail.com>
2024-02-07 20:55:25 -0300

    first commit

---
+++ src/main/java/com/mjv/curso/myfristcrud/MyFristCrudApplication.java
@@ -0,0 +1,13 @@
+package com.mjv.curso.myfristcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFristCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFristCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mjv/curso/myfristcrud/MyFristCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjv.curso.myfristcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFristCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
