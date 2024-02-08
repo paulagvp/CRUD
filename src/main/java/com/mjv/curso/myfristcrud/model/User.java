488c28c506c0d83745ebd84c988c09785caf42ad
Paula <paulagvp@gmail.com>
2024-02-08 18:26:13 -0300

    Iniciando a criação da classe USER

--- src/main/java/com/mjv/curso/myfristcrud/model/User.java
+++ src/main/java/com/mjv/curso/myfristcrud/model/User.java
@@ -1,4 +1,29 @@
 package com.mjv.curso.myfristcrud.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_usuario")
 public class User {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "nome")
+    private String name;
+
+    @Column(name = "nome_completo")
+    private String fullName;
+
+    @Column(name = "idade")
+    private Integer age;
+
+    @Column(name = "e-mail")
+    private String email;
+
+    @Column(name = "senha")
+    private String password;
+
 }
