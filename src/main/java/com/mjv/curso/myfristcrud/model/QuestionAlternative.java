59e7471685f8420f386feb8c6949b7226f604332
Paula <paulagvp@gmail.com>
2024-02-08 21:34:27 -0300

    Atualização

--- src/main/java/com/mjv/curso/myfristcrud/model/QuestionAlternative.java
+++ src/main/java/com/mjv/curso/myfristcrud/model/QuestionAlternative.java
@@ -1,4 +1,18 @@
 package com.mjv.curso.myfristcrud.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import org.springframework.util.AlternativeJdkIdGenerator;
+
 public class QuestionAlternative {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String alternative;
+    private boolean corretAlternative;
+
+    private List<QuestionAlternative> questionAlternativeList;
+
 }
